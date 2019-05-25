42d56d6dededcdf925ebafda02fcb27202be011c
Joseph Holland <jh2044@cam.ac.uk>
2019-05-25 10:42:48 +0100

    cleanup of files

---
+++ week 1/process_linearise.m
@@ -0,0 +1,14 @@
+% Linearisation of evaporator model
+
+% Linearise model about (L2,X2,P2) = (25,50.5,1) 
+% and (F1,F2,X1,T1,P100,F3,T200,F200) = (10,2,5,40,194.7,50,25,208)
+[evapA1,evapB1,evapC1,evapD1] = linmod('process_sub',[25,50.5,1],[10,2,5,40,194.7,50,25,208]);
+
+% Extract state poles% 
+% % Linearise model about (X2,P2) = (25,50.5) 
+% % and (F1,F2,X1,F5,Q100,T1) = (10,1,1,1,1,1)
+% [evapA2,evapB2,evapC2,evapD2] = linmod('evaporator',[25,50.5],[10,1,1,1,1,1]);
+% 
+% % Extract state poles
+% eigA2 = eig(evapA2);
+eigA1 = eig(evapA1);
---
+++ week 2/process_linearise_PI_phase.m
@@ -0,0 +1,61 @@
+% Week 2 linearise
+
+% steady-state state guess
+X0 = [25,50.5,194.7,1,208,2];
+
+X1 = [1,25,50.5,194.7,208,2];
+% steady-state input guess
+U0 = [10,2,5,40,194.7,50,25,208];
+
+% obtain real steady-state conditions with fixed input
+[X,U,Y,DX] = trim('process_wk2_linearise',X0',U0',[],[],[1;2;3;4;5;6;7;8],[]);
+
+% obtain linearised model about equilibrium
+[procA1,procB1,procC1,procD1] = linmod('process_wk2_linearise',X,U);
+
+% extract 2nd col of B (F2 in) and 6th row of C (L2 output), and (2,6)
+% element of D (F2->L2)
+Bnew = procB1(:,2);
+Cnew = procC1(6,:);
+Dnew = procD1(6,2);
+
+% set up a range of systems
+sys1_pos = ss(procA1,Bnew,Cnew,Dnew);
+sys1 = ss(procA1,-Bnew,Cnew,Dnew);
+sys14_2 = ss(procA1,-14.2*Bnew,Cnew,Dnew);
+
+% model from heuristic development
+sys65 = ss(procA1,-65*Bnew,Cnew,Dnew);
+
+% from the various systems bode plots may be generated to find phase margin
+
+% given that the system with K_p = 14.2 provides a phase margin of 45deg,
+% we use that value in our system.
+% In order to develop a PI controller which sets the phase margin to 40deg
+% at 0.604rad/s (found from margin plot) we set the phase of the controller
+% to -5deg at this frequency
+
+% set up controller transfer function
+
+omega = 0.604i;
+K_p = 14.2;
+syms T_i;
+
+% setup controller transfer function
+control_tf = angle((T_i*omega + 1)/(omega*T_i));
+% solve with an initial close guess (this avoids complex solns)
+vpasolve(control_tf==-0.087266,T_i,18)
+
+% determine if bode plot is correct (0dB crossover @ ~0.604rad/s with phase
+% margin of ~ 40deg)
+
+% set up transfer function variable
+s = tf('s');
+
+% linearised system transfer function
+G_tf = -Cnew*inv(s*eye(6) - procA1)*Bnew
+
+% controller tranfer function
+K_tf = K_p * (1 + 1/(s*18.924));
+
+margin(K_tf*G_tf)
