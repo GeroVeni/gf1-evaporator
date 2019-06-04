997f0001cd99a041b56beef882179aa4bfc377bb
GeroVeni <geroveni@gmail.com>
2019-06-04 08:31:16 +0100

    Late changes

--- week 3/binsrc.m
+++ week 3/binsrc.m
@@ -1,16 +1,8 @@
-% out_names = ["X2", "P2", "T2", "T3", "F4", "L2", "T100", "Q100", "F100", "Q200", "T201", "F5"];
-% controlled = [1, 2, 6];
-% out_saturated = [2, 5, 6, 12];
-% state_names=["X2", "P2", "P100", "L2", "F200", "F2_nolag", "F200_nolag", "P100_nolag", "F2"];
-% state_saturated = [3, 5, 9];
-% % Saturation limits correspond to saturated outputs concatenated with
-% % saturated states
-% ss_vals = [25, 50.5, 1];
-% sat_lims = [100, 16, 2, 16, 400, 416, 4];
+% function [opt_pos, opt_neg] = binsrc(sig_id, start_pos, start_neg)
+% consts;
 
 steps = zeros(1, 8);
-step_id = 8;
-hi = -25;
+hi = start_pos;
 lo = 0;
 while abs(hi-lo) > 10^(-2)
     steps(step_id) = (hi + lo) / 2;
@@ -19,10 +11,28 @@ while abs(hi-lo) > 10^(-2)
     checkres;
     
     if (err_found)
-        % Doesnt work => decrease gain
+        % Doesnt work => decrease gain
         hi = steps(step_id);
     else
         lo = steps(step_id);
     end
 end
-lo
+out_pos = lo;
+
+steps = zeros(1, 8);
+hi = start_neg;
+lo = 0;
+while abs(hi-lo) > 10^(-2)
+    steps(step_id) = (hi + lo) / 2;
+    
+    % Runs a check to see if there are any ss error / saturations
+    checkres;
+    
+    if (err_found)
+        % Doesnt work => decrease gain
+        hi = steps(step_id);
+    else
+        lo = steps(step_id);
+    end
+end
+out_neg = lo;
--- week 3/process_parameter_adjustment.m
+++ week 3/process_parameter_adjustment.m
@@ -22,37 +22,6 @@ UA2 = 6.84;
 
 %% SIMULATE SYSTEM AND GATHER PERFORMATIVE MEASURES
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 out_names = ["X2", "P2", "T2", "T3", "F4", "L2", "T100", "Q100", "F100", "Q200", "T201", "F5"];
 controlled = [1, 2, 6];
 out_saturated = [2, 5, 6, 12];
@@ -63,10 +32,30 @@ state_saturated = [3, 5, 9];
 ss_vals = [25, 50.5, 1];
 sat_lims = [100, 16, 2, 16, 400, 416, 4];
 
+tot = 8*4*3*4;
+cnt = 0;
+err_cnt = 0;
 for rho_A = 10:10:40
     for M = 15:5:25
         for UA2 = 5:8
-            ...
+            for step_pos = 1:8
+                disp("Progress: " + 100 * cnt / tot)
+                steps = zeros(1, 8);
+                steps(step_pos) = max_high(step_pos) / 2;
+                checkres;
+                cnt = cnt + 1;
+                if (err_found)
+                    err_cnt = err_cnt + 1;
+                    disp("Error: " + rho_A + " " + M + " " + UA2 + " " + step_pos)
+                end
+                
+                steps(step_pos) = max_low(step_pos) / 2;
+                checkres;
+                if (err_found)
+                    err_cnt = err_cnt + 1;
+                    disp("Error: " + rho_A + " " + M + " " + UA2 + " " + step_pos)
+                end
+            end
         end
     end
 end
